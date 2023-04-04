bc7a6b8a5d21751e5bb6f74c68cabcce9e62d32b
mik A <akimgri44@gmail.com>
2023-04-04 19:10:54 +0300

    adding user

--- stuff.py
+++ stuff.py
@@ -2,6 +2,7 @@ import requests as r
 import geopy
 import os
 from datetime import datetime
+import sqlite3
 
 
 def git_search(query, language='python'):
@@ -41,3 +42,20 @@ def get_forecast(lat, lon):
     }
     resp = r.get(url, params=params).json()
     return resp
+
+
+def connect_db():
+    con = sqlite3.connect("bot_db.sqlite")
+    return con
+
+
+def add_user(f_name, l_name, tg_name, phone, connection):
+    sql = f'''ISERT INTO users (first_name, last_name, tg_id, phone_number)
+            VALUES ({f_name}, {l_name}, {phone})'''
+    try:
+        curs = connection.cursor()
+        curs.execute(sql)
+        return 0
+    except:
+        return None
+
